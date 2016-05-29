ea3b284063d76a871b50412b27f2caa6b0b5c5ce
hayifeng <13667693152@163.com>
2016-05-29 20:41:15 +0800

    完成实验2

--- Exercise_02/app/src/main/java/com/example/wulian/exercise_02/MainActivity.java
+++ Exercise_02/app/src/main/java/com/example/wulian/exercise_02/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.wulian.exercise_02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        final EditText editTextAccount = (EditText) findViewById(R.id.editTextAccount);
+        final EditText editTextPassword = (EditText) findViewById(R.id.editTextPassWord);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editTextAccount.getText().toString();
+                String passwd = editTextPassword.getText().toString();
+                Toast.makeText(MainActivity.this, "姓名："+name+"密码："+passwd, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
